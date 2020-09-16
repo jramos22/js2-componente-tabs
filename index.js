4cb42c8edb0c56d0ab0f08c666016235ffee72a7
José Daniel Ramos Barquero <josedanielramosbarquero@gmail.com>
2020-09-15 19:33:11 -0600

    arego funcion anonima y cambios de estilos en el css

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-function evento1(event) {
-    event.preventDefault();
-    const id = event.currentTarget.getAttribute('href').substring(1);
-
-    for (let i= 0; i < contenido.length; i++) {
-        contenido[i].classList.add('js_content_hidden');
-        if (contenido[i].getAttribute('id')==id) {
-            contenido[i].classList.remove('js_content_hidden');
-            }
-        }
-    for (let i = 0; i < enlaces.length; i++) {
-        enlaces[i].classList.remove('js_active');
-    }
-    event.currentTarget.classList.add('js_active');
-}
-
 const enlaces = document.querySelectorAll('.tabs-tablist-item');
 
 const contenido =document.querySelectorAll('.tabs-content-item');
@@ -27,5 +11,19 @@ contenido[0].classList.remove('js_content_hidden');
 enlaces[0].classList.add('js_active');
 
 for(let i = 0; i<enlaces.length; i++){
-    enlaces[i].addEventListener('click', evento1);
+    enlaces[i].addEventListener('click', (event)=>{
+        event.preventDefault();
+    const id = event.currentTarget.getAttribute('href').substring(1);
+
+    for (let i= 0; i < contenido.length; i++) {
+        contenido[i].classList.add('js_content_hidden');
+        if (contenido[i].getAttribute('id')==id) {
+            contenido[i].classList.remove('js_content_hidden');
+            }
+        }
+    for (let i = 0; i < enlaces.length; i++) {
+        enlaces[i].classList.remove('js_active');
+    }
+    event.currentTarget.classList.add('js_active');
+    });
 }
